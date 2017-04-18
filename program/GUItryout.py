5fc0e59ad7495ab1e057e1463ad76faa931ccc3e
Oleksiy1990 <oleksiy1990@gmail.com>
2017-04-19 00:38:13 +0200

    Small changes to GUI
    Trying to get a better approach to PyQT4

--- program/GUItryout.py
+++ program/GUItryout.py
@@ -40,86 +40,6 @@ class MyWindowClass(QtGui.QMainWindow):
     def timerfunc(self):
         self.ui.ErrorMessages.append(time.asctime( time.localtime(time.time())))
         
-
-
-    
-    # def fitting(self,data):
-    #     """
-    #     This function does the fitting along the 2 axes independently (using integrated  
-    #     images along axes), and it can be extended to do the full 2D fitting.
-
-    #     The "data" input is the 2D numpy array representing the image. The idea is that 
-    #     this can be anything, most importantly the data coming from an Ethernet cam
-
-    #     """
-    #     print("Doing fitting")
-    #     #print(data.shape)
-    #     scale_pixels_mm = self.ui.pixelSizeInput.value()*10e-3
-    
-    #     # Do the fits along each axis. Try if it works and show an error message if it fails
-    #     try:
-    #         fit0 = frt.fit_axis(data,1) # NOTE! Axes and array indexing are messed up
-    #         fit1 = frt.fit_axis(data,0)
-    #     except:
-    #         self.ui.FitResultsHorizontal.setPlainText("")
-    #         self.ui.FitResultsVertical.setPlainText("")
-    #         self.ui.ErrorMessages.setPlainText("Fitting failed!")
-
-    #         #canvas_forbeam = self.ui.BeamDisplay.canvas
-    #         #canvas_forbeam.ax.pcolorfast(data)
-    #         #canvas_forbeam.draw()
-
-    #         return None #if there's an error, we stop the procedure
-           
-        
-    #     # Read out and display fit parameters along horiz. axis
-    #     fitted_params_horiz = fit0[2].params.valuesdict()
-    #     axis0_pos = fitted_params_horiz["r_zero"]*scale_pixels_mm
-    #     axis0_omega = fitted_params_horiz["omega_zero"]*scale_pixels_mm
-    #     self.ui.FitResultsHorizontal.setPlainText("")
-    #     self.ui.FitResultsHorizontal.append("Units: mm")
-    #     self.ui.FitResultsHorizontal.append("Peak position %.3f" % axis0_pos)
-    #     self.ui.FitResultsHorizontal.append("Beam width %.3f" % axis0_omega)
-    #     #self.ui.FitResultsHorizontal.append("Height above bgr %.3f" % fitted_params_horiz["I_zero"])
-
-    #     # Read out and display fit parameters along horiz. axis
-    #     fitted_params_vert = fit1[2].params.valuesdict()
-    #     axis1_pos = fitted_params_vert["r_zero"]*scale_pixels_mm
-    #     axis1_omega = fitted_params_vert["omega_zero"]*scale_pixels_mm
-    #     self.ui.FitResultsVertical.setPlainText("")
-    #     self.ui.FitResultsVertical.append("Units: mm")
-    #     self.ui.FitResultsVertical.append("Peak position %.3f" % axis1_pos)
-    #     self.ui.FitResultsVertical.append("Beam width %.3f" % axis1_omega)
-    #     #self.ui.FitResultsVertical.append("Height above bgr %.2f" % fitted_params_vert["I_zero"])
-
-
-    #     # In principle this can function as a powermeter, but it
-    #     # needs to be calibrated first
-    #     total_power = np.sum(data) 
-    #     self.ui.PowerMeter.display(total_power*10e-9)
-
-        
-        
-    #     # Showing the beam profile itself 
-    #     canvas_forbeam = self.ui.BeamDisplay.canvas
-    #     canvas_forbeam.ax.pcolorfast(data)
-    #     canvas_forbeam.draw()
-
-    #     # Showing the horizontal axis fit (needs to be improved)
-    #     canvas_forHorizFit = self.ui.xplot.canvas
-    #     canvas_forHorizFit.ax.plot(fit0[0],fit0[1],"b-",fit0[0],mm.residual_G1D(fit0[2].params,fit0[0]),"r-")
-    #     canvas_forHorizFit.ax.xaxis.set_visible(False) #for now remove axis labeling, it's wrong anyway
-    #     canvas_forHorizFit.ax.yaxis.set_visible(False)
-    #     canvas_forHorizFit.draw()
-
-    #     # Showing the vertical axis fit (needs to be improved)
-    #     canvas_forVertFit = self.ui.yplot.canvas
-    #     canvas_forVertFit.ax.plot(fit1[1],fit1[0],"b-",mm.residual_G1D(fit1[2].params,fit1[0]),fit1[0],"r-")
-    #     canvas_forVertFit.ax.xaxis.set_visible(False) #for now remove axis labeling, it's wrong anyway
-    #     canvas_forVertFit.ax.yaxis.set_visible(False)
-    #     canvas_forVertFit.draw()
-
-
     def plotting(self,fitresults_tuple):
         
         fit0 = fitresults_tuple[0]
@@ -332,5 +252,5 @@ if __name__ == '__main__':
 
     #sys.path.append("/Users/oleksiy/Desktop/PythonCode/beamFitter/GUIdesign")
     main()
-    sys.exit(0)
+    #sys.exit(0)
 
